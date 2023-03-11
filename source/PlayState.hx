0794733c55b8478a2e3f5fc34e7cd7329144d354
Fyrid <jkeonde@gmail.com>
2023-03-11 00:38:30 -0500

    cam fix

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -85,16 +85,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X = 42;
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
-	public static var ratingStuff:Array<Dynamic> = [ // fyrid if you wanna change this to alphabetical ratings just lmk -frogb
-		['JESUS PLAY A DIFFERENT SONG', 0.2], //From 0% to 19%
-		['How did you even get here', 0.4], //From 20% to 39%
-		['skill issue', 0.5], //From 40% to 49%
-		['Eh', 0.6], //From 50% to 59%
-		['Okay...', 0.69], //From 60% to 68%
-		['sex', 0.7], //69%
-		['Amazing', 0.8], //From 70% to 79%
-		['Epic', 0.9], //From 80% to 89%
-		['Outstanding!', 1], //From 90% to 99%
+	public static var ratingStuff:Array<Dynamic> = [
+		['You Suck!', 0.2], //From 0% to 19%
+		['Shit', 0.4], //From 20% to 39%
+		['Bad', 0.5], //From 40% to 49%
+		['Bruh', 0.6], //From 50% to 59%
+		['Meh', 0.69], //From 60% to 68%
+		['Nice', 0.7], //69%
+		['Good', 0.8], //From 70% to 79%
+		['Great', 0.9], //From 80% to 89%
+		['Sick!', 1], //From 90% to 99%
 		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	public static var animatedShaders:Map<String, DynamicShaderHandler> = new Map<String, DynamicShaderHandler>();
@@ -157,7 +157,7 @@ class PlayState extends MusicBeatState
 	public var gf:Character = null;
 	public var boyfriend:Boyfriend = null;
 
-	var funnyFloatyBoys:Array<String> = ['dave-3d', 'mordon', 'heldai-phase-1', 'bambiGod', 'sbarren']; // added shitted barren here -frogb
+	var funnyFloatyBoys:Array<String> = ['dave-3d', 'mordon', 'heldai-phase-1']; // i know 404 is open useable but if we want to add him to our mod then we can put his json name here. -frogb
 	var funnySideFloatyBoys:Array<String> = ['bombu', 'bombu-expunged'];
 	var canSlide:Bool = true;
 	
@@ -1609,7 +1609,7 @@ class PlayState extends MusicBeatState
 		}
 		
 		var engineName:String = 'Psych';
-		engineName = MainMenuState.engineVers[FlxG.random.int(0, MainMenuState.engineVers.length)]; // oh my fucking GOD FYRID WHY- -frogb
+		engineName = MainMenuState.engineVers[FlxG.random.int(0, MainMenuState.engineVers.length)];
 
 		var kadeEngineWatermark = new FlxText(17, FlxG.height - 24, 0, SONG.song + " - " +  engineName + " Engine (PE 0.6.2)", 16);
 		kadeEngineWatermark.setFormat(Paths.font("comic.ttf"), 16, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
@@ -1618,12 +1618,14 @@ class PlayState extends MusicBeatState
 		add(kadeEngineWatermark);
 
 		var composersWatermark:String;
-		switch (SONG.song.toLowerCase()) // note to self: never release source till full release // this was kinda sampled from geometric vocation wouldnt this be stealing code? lol -frogb
+		switch (SONG.song.toLowerCase()) // note to self: never release source till full release
 		{
 			
-			// PLACEHOLDER songs
-			//i deleted donut haha -frogb
-			case 'meadow' | 'ringularity' : // ayo renamed it cuz of donut smh //FYRID I WAS DOING YOU A FAVOR -frogb
+			// PLACEHOLDER SONGS
+			case 'blast' :
+				composersWatermark = 'Donut'; // idiot
+				// composersWatermark = 'MoiMoi'; <-- for when i end up putting moimoi's 4 minute blast in-game
+			case 'meadow' | 'ringularity' : // ayo renamed it cuz of donut smh
 				composersWatermark = 'Ayo';
 			// add fyrid's songs here
 			case 'midnight' | 'remorseless' | 'all-star' :
@@ -3443,29 +3445,6 @@ class PlayState extends MusicBeatState
 
 		switch (curStage)
 		{
-			case 'houseNight' | 'farmNight': // Dark character thing
-                {
-                    dad.color = 0xFF878787;
-                    gf.color = 0xFF878787;
-                    boyfriend.color = 0xFF878787;
-
-					if (SONG.player2 == 'bambi-god2d')
-					{
-						dad.color = 0xFFFFFFFF;
-					}
-                }
-			case 'spooky': // Darker character thing
-				{
-					dad.color = 0xFF383838;
-					gf.color = 0xFF383838;
-					boyfriend.color = 0xFF383838;
-				}
-			case 'farmSunset' | 'houseSunset': // sunset !!
-				{
-					dad.color = 0xFFFF8F65;
-					gf.color = 0xFFFF8F65;
-		    		boyfriend.color = 0xFFFF8F65;
-				}
 			case 'tank':
 				moveTank(elapsed);
 			case 'schoolEvil':
@@ -4518,15 +4497,15 @@ for (key => value in luaShaders)
 		if(isDad)
 		{
 			camFollow.set(dad.getMidpoint().x + 150, dad.getMidpoint().y - 100);
-			camFollow.y += dadCamFollowY;
-			camFollow.x += dadCamFollowX;
+			camFollow.x += dad.cameraPosition[0] + opponentCameraOffset[0];
+			camFollow.y += dad.cameraPosition[1] + opponentCameraOffset[1];
 			tweenCamIn();
 		}
 		else
 		{
 			camFollow.set(boyfriend.getMidpoint().x - 100, boyfriend.getMidpoint().y - 100);
-			camFollow.x -= camFollowX;
-			camFollow.y += camFollowY;
+			camFollow.x -= boyfriend.cameraPosition[0] - boyfriendCameraOffset[0];
+			camFollow.y += boyfriend.cameraPosition[1] + boyfriendCameraOffset[1];
 
 			if (Paths.formatToSongPath(SONG.song) == 'tutorial' && cameraTwn == null && FlxG.camera.zoom != 1)
 			{
@@ -5263,24 +5242,16 @@ for (key => value in luaShaders)
 			var char:Character = dad;
 			var animToPlay:String = '';
 			switch (Math.abs(note.noteData))
-				{
-					case 0:
-						animToPlay = 'singLEFT';
-						dadCamFollowY = 0;
-						dadCamFollowX = -25;
-					case 1:
-						animToPlay = 'singDOWN';
-						dadCamFollowY = 25;
-						dadCamFollowX = 0;
-					case 2:
-						animToPlay = 'singUP';
-						dadCamFollowY = -25;
-						dadCamFollowX = 0;
-					case 3:
-						animToPlay = 'singRIGHT';
-						dadCamFollowY = 0;
-						dadCamFollowX = 25;
-				}
+			{
+				case 0:
+					animToPlay = 'singLEFT';
+				case 1:
+					animToPlay = 'singDOWN';
+				case 2:
+					animToPlay = 'singUP';
+				case 3:
+					animToPlay = 'singRIGHT';
+			}
 
 			if(note.gfNote) {
 				char = gf;
@@ -5291,7 +5262,7 @@ for (key => value in luaShaders)
 				char.playAnim(animToPlay, true);
 				char.holdTimer = 0;
 
-				var camPower = 60;
+				var camPower = 30;
 				moveCameraSection();
 				if (!SONG.notes[curSection].mustHitSection) {
 					switch (note.noteData) {
@@ -5388,20 +5359,12 @@ for (key => value in luaShaders)
 				{
 					case 0:
 						animToPlay = 'singLEFT';
-						camFollowY = 0;
-						camFollowX = -25;
 					case 1:
 						animToPlay = 'singDOWN';
-						camFollowY = 25;
-						camFollowX = 0;
 					case 2:
 						animToPlay = 'singUP';
-						camFollowY = -25;
-						camFollowX = 0;
 					case 3:
 						animToPlay = 'singRIGHT';
-						camFollowY = 0;
-						camFollowX = 25;
 				}
 
 				if(note.gfNote)
@@ -5831,17 +5794,14 @@ for (key => value in luaShaders)
 		{
 	    	if (curBeat % boyfriend.danceEveryNumBeats == 0 && boyfriend.animation.curAnim != null && !boyfriend.animation.curAnim.name.startsWith('sing') && !boyfriend.stunned)
 	     	{
-				camFollowX = 0;
-		    	camFollowY = 0;
 		    	boyfriend.dance();
+
 				boyfriend.playAnim('idle', true);
 	    	}
 		}
 		if (curBeat % dad.danceEveryNumBeats == 0 && dad.animation.curAnim != null && !dad.animation.curAnim.name.startsWith('sing') && !dad.stunned)
 		{
 			dad.dance();
-			dadCamFollowX = 0;
-		    dadCamFollowY = 0;
 		}
 
 		switch (curStage)
